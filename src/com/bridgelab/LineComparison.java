d06b780b0af5d1644543bd1f3979887535c9088d
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-10-19 21:00:20 +0530

    Welcome to the Line Comparison Problem by using Java

---
+++ src/com/bridgelab/LineComparison.java
@@ -0,0 +1,7 @@
+package com.bridgelab;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Line Comparison Problem by using Java");
+    }
+}
